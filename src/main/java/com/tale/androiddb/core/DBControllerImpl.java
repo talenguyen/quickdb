5ae6243edc49530965f2dada564052d83951da40
Tale Nguyen <giangnguyen.tale@gmail.com>
2014-07-08 12:42:06 +0700

    * fix: "order by ... asd" -> "asc"

--- src/main/java/com/tale/androiddb/core/DBControllerImpl.java
+++ src/main/java/com/tale/androiddb/core/DBControllerImpl.java
@@ -36,24 +36,11 @@ class DBControllerImpl implements DBController {
         }
     }
 
-    /**
-     * Convenience method for inserting a row into the database.
-     *
-     * @param table  The name of table to insert
-     * @param values ContentValue object to insert.
-     * @return the row ID of the newly inserted row, or -1 if an error occurred
-     */
     public long insert(String table, ContentValues values) {
         return insert(table, null, values);
     }
 
-    /**
-     * Convenience method for inserting a row into the database.
-     *
-     * @param table  The name of table to insert
-     * @param values ContentValue object to insert.
-     * @return the row ID of the newly inserted row, or -1 if an error occurred
-     */
+
     @Override
     public long insert(String table, String nullColumnHack, ContentValues values) {
         // Verify pre-define.
@@ -62,18 +49,7 @@ class DBControllerImpl implements DBController {
         return database.insert(table, null, values);
     }
 
-    /**
-     * Convenience method for updating rows in the database.
-     *
-     * @param values      a map from column names to new column values. null is a valid
-     *                    value that will be translated to NULL.
-     * @param whereClause the optional WHERE clause to apply when updating. Passing null
-     *                    will update all rows.
-     * @param whereArgs   You may include ?s in the where clause, which will be replaced
-     *                    by the values from whereArgs. The values will be bound as
-     *                    Strings.
-     * @return the number of rows affected
-     */
+
     @Override
     public int update(String tableName, ContentValues values, String whereClause, String[] whereArgs) {
         // Verify pre-define.
@@ -83,15 +59,6 @@ class DBControllerImpl implements DBController {
         return database.update(tableName, values, whereClause, whereArgs);
     }
 
-    /**
-     * @param tableName   the table to delete from
-     * @param whereClause the optional WHERE clause to apply when deleting. Passing null
-     *                    will delete all rows.
-     * @param whereArgs   You may include ?s in the where clause, which will be replaced
-     *                    by the values from whereArgs. The values will be bound as
-     *                    Strings.
-     * @return the number of rows affected
-     */
     @Override
     public int delete(String tableName, String whereClause, String[] whereArgs) {
         verify();
@@ -99,20 +66,6 @@ class DBControllerImpl implements DBController {
         return database.delete(tableName, whereClause, whereArgs);
     }
 
-    /**
-     * @param distinct      true if you want each row to be unique, false otherwise.
-     * @param table         The table name to compile the query against.
-     * @param columns       A list of which columns to return. Passing null will return all columns, which is discouraged to prevent reading data from storage that isn't going to be used.
-     * @param selection     A filter declaring which rows to return, formatted as an SQL WHERE clause (excluding the WHERE itself). Passing null will return all rows for the given table.
-     * @param selectionArgs You may include ?s in selection, which will be replaced by the values from selectionArgs, in order that they appear in the selection. The values will be bound as Strings.
-     * @param groupBy       A filter declaring how to group rows, formatted as an SQL GROUP BY clause (excluding the GROUP BY itself). Passing null will cause the rows to not be grouped.
-     * @param having        A filter declare which row groups to include in the cursor, if row grouping is being used, formatted as an SQL HAVING clause (excluding the HAVING itself). Passing null will cause all row groups to be included, and is required when row grouping is not being used.
-     * @param orderBy       How to order the rows, formatted as an SQL ORDER BY clause (excluding the ORDER BY itself). Passing null will use the default sort order, which may be unordered.
-     * @param limit         Limits the number of rows returned by the query, formatted as LIMIT clause. Passing null denotes no LIMIT clause.
-     * @return A Cursor object, which is positioned before the first entry. Note
-     * that Cursors are not synchronized, see the documentation for more
-     * details.
-     */
     @Override
     public Cursor query(boolean distinct, String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy, String limit) {
         verify();
@@ -140,36 +93,11 @@ class DBControllerImpl implements DBController {
                 selectionArgs, groupBy, having, orderBy, limit);
     }
 
-    /**
-     * Query the given URL, returning a Cursor over the result set.
-     *
-     * @param table         The table name to compile the query against.
-     * @param selection     A filter declaring which rows to return, formatted as an SQL
-     *                      WHERE clause (excluding the WHERE itself). Passing null will
-     *                      return all rows for the given table.
-     * @param selectionArgs You may include ?s in selection, which will be replaced by the
-     *                      values from selectionArgs, in order that they appear in the
-     *                      selection. The values will be bound as Strings.
-     * @return A Cursor object, which is positioned before the first entry. Note
-     * that Cursors are not synchronized, see the documentation for more
-     * details.
-     */
     @Override
     public Cursor quickQuery(String table, String selection, String[] selectionArgs) {
-        return query(false, table, null, selection, selectionArgs, null, null, "_id asd", null);
+        return query(false, table, null, selection, selectionArgs, null, null, "_id asc", null);
     }
 
-    /**
-     * Runs the provided SQL and returns a Cursor over the result set.
-     *
-     * @param sql           the SQL query. The SQL string must not be ; terminated
-     * @param selectionArgs You may include ?s in where clause in the query, which will be
-     *                      replaced by the values from selectionArgs. The values will be
-     *                      bound as Strings.
-     * @return A Cursor object, which is positioned before the first entry. Note
-     * that Cursors are not synchronized, see the documentation for more
-     * details.
-     */
     @Override
     public Cursor rawQuery(String sql, String[] selectionArgs) {
         verify();
